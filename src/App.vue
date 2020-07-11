292fccecbbf97b3d967b78a35bd4231099f0b894
MohammedKhalid96 <mohammedkhalid96@outlook.com>
2020-07-11 15:29:07 +0200

    drag drop task

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.blank-div {
+  width: 100%;
+  height: 350px;
+  background: #ccc;
+  border: 2px solid #fff;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <VueDraggable />
+  </div>
+</template>
+
+<script>
+import VueDraggable from "../components/VueDraggable";
+
+export default {
+  name: "HelloWorld",
+
+  components: {
+    VueDraggable
+  },
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+</style>
